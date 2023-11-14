4221b1a456141027d9a0b4f1d0a20b935bd538cc
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2023-11-14 00:04:25 -0300

    docs: aprendendo sobre ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgclassComponent } from './shared/components/diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgforComponent } from './shared/components/diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgifComponent } from './shared/components/diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './shared/components/diretiva-ngswitch/diretiva-ngswitch.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DiretivaNgforComponent } from './shared/components/diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
-  declarations: [AppComponent, DiretivaNgifComponent, DiretivaNgswitchComponent, DiretivaNgforComponent],
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
