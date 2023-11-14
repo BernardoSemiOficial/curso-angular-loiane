b359f7e8492f0a48bf7956d7da4cb33cfd221e95
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2023-11-13 23:23:24 -0300

    docs: aprendendo sobre ngFor e ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './shared/components/diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './shared/components/diretiva-ngswitch/diretiva-ngswitch.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DiretivaNgforComponent } from './shared/components/diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
-  declarations: [AppComponent, DiretivaNgifComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, DiretivaNgifComponent, DiretivaNgswitchComponent, DiretivaNgforComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
