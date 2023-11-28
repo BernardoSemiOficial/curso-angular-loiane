30f0757f36d375c46489596a576e1cb40c981a80
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2023-11-27 22:42:56 -0300

    docs: configurando os routers links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './shared/components/login/login.component';
-import { HomeComponent } from './shared/components/home/home.component';
 import { CursosComponent } from './shared/components/cursos/cursos.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    CursosComponent
+    CursosComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
