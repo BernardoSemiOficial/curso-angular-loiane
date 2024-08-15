9b3e1121612b9aea3d6b967be16a9ebbe54b2c1a
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-08-15 19:22:05 -0300

    docs: voltando a versão do componente form-control-field para funcional

--- 08-forms/src/app/components/form-control-field/form-control-field.component.ts
+++ 08-forms/src/app/components/form-control-field/form-control-field.component.ts
@@ -1,12 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, forwardRef, Input } from '@angular/core';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormsModule,
-  NG_VALUE_ACCESSOR,
-  NgModel,
-} from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormsModule, NgModel } from '@angular/forms';
 
 export enum MessageType {
   alertWarning = 'alert-warning',
@@ -18,24 +12,15 @@ export enum MessageType {
   selector: 'app-form-control-field',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => FormControlFieldComponent),
-      multi: true,
-    },
-  ],
   templateUrl: './form-control-field.component.html',
   styleUrl: './form-control-field.component.scss',
 })
-export class FormControlFieldComponent implements ControlValueAccessor {
+export class FormControlFieldComponent {
   @Input() formControlField!: NgModel | AbstractControl | null;
   @Input() fieldName!: string;
   @Input() label!: string;
   @Input() type: string = 'text';
   @Input() message!: { [key: string]: { text: string; type: MessageType } };
-  controlValue: any;
-  isDisabled: boolean = false;
 
   analyticsFormControl() {
     const formControl = this.formControlField;
@@ -47,23 +32,5 @@ export class FormControlFieldComponent implements ControlValueAccessor {
       errorKey: errorsKey[0],
       status: formControl?.status?.toLowerCase() ?? '',
     };
-  }
-
-  _onChange = (_: any) => {};
-  _onTouched = () => {};
-
-  writeValue(value: any): void {
-    if (value !== this.controlValue) {
-      this.controlValue = value;
-    }
-  }
-  registerOnChange(fn: (_: any) => void): void {
-    this._onChange = fn;
-  }
-  registerOnTouched(fn: () => void): void {
-    this._onTouched = fn;
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    this.isDisabled = isDisabled;
   }
 }
