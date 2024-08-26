f1dfaf477e37134da0c640b429e6140a6ca74088
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-08-26 08:52:35 -0300

    docs: utilizado componente de alerta em casos de error

--- 09-requests-http/src/app/modules/cursos/components/cursos-lista/cursos-lista.component.ts
+++ 09-requests-http/src/app/modules/cursos/components/cursos-lista/cursos-lista.component.ts
@@ -1,25 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { AlertModalComponent } from '../../../../shared/alert-modal/alert-modal.component';
 import { Curso } from '../../models/cursos.model';
 import { CursosService } from '../../services/cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, AlertModalComponent],
   templateUrl: './cursos-lista.component.html',
 })
 export class CursosListaComponent {
+  @ViewChild(AlertModalComponent) alertModal!: AlertModalComponent;
   private cursosService: CursosService = inject(CursosService);
-  cursos$: Observable<Curso[]> = this.cursosService.getCursos();
+  cursos$: Observable<Curso[]> = this.cursosService.getCursos().pipe(
+    catchError(() => {
+      this.handleError();
+      return of([]);
+    })
+  );
 
   deleteCurso(cursoId: number) {
     console.log(cursoId);
   }
 
   getCursos() {
-    this.cursosService.getCursos().subscribe();
+    this.cursosService
+      .getCursos()
+      .pipe(
+        catchError(() => {
+          this.handleError();
+          return of([]);
+        })
+      )
+      .subscribe();
+  }
+
+  handleError() {
+    this.alertModal.pushAlert({
+      type: 'danger',
+      msg: 'Erro ao carregar cursos',
+    });
   }
 }
